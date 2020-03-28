b537fe9ac746d9ad9873601a3ad4a9c47a8b967e
Paul <bujor.paul2000@gmail.com>
2020-03-28 16:12:30 +0100

    Update ChatViewModel.java

--- POMS_Client/src/main/java/dk/via/viewmodel/ChatViewModel.java
+++ POMS_Client/src/main/java/dk/via/viewmodel/ChatViewModel.java
@@ -15,109 +15,75 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
-public class ChatViewModel implements PropertyChangeListener
-{
-  private Model model;
-  private StringProperty username;
-  private IntegerProperty connectedUsers;
-  private StringProperty ip;
-  private ObservableList<TableRowData> list;
-
-  public ChatViewModel(Model model)
-  {
-    this.model = model;
-    username = new SimpleStringProperty(model.getUsername());
-    connectedUsers = new SimpleIntegerProperty(model.getConnectedUsers());
-    ip = new SimpleStringProperty(model.getIP());
-    list = createList();
-    model.addListener(this);
-  }
-
-
-  public StringProperty usernameProperty()
-  {
-    return username;
-  }
-
-  public IntegerProperty connectedUsersProperty()
-  {
-    return connectedUsers;
-  }
+public class ChatViewModel implements PropertyChangeListener {
+    private Model model;
+    private StringProperty username;
+    private IntegerProperty connectedUsers;
+    private StringProperty ip;
+    private ObservableList<TableRowData> list;
 
     public ChatViewModel(Model model) {
         this.model = model;
-        username = new SimpleStringProperty();
-        connectedUsers = new SimpleIntegerProperty();
-        ip = new SimpleStringProperty();
+        username = new SimpleStringProperty(model.getUsername());
+        connectedUsers = new SimpleIntegerProperty(model.getConnectedUsers());
+        ip = new SimpleStringProperty(model.getIP());
+        list = createList();
         model.addListener(this);
     }
 
-
-  @Override public void propertyChange(PropertyChangeEvent evt)
-  {
-    if (evt.getPropertyName().equals("connectedUpdate"))
-    {
-      Platform.runLater(() -> connectedUsers.set(model.getConnectedUsers()));
-    }
-    else if (evt.getPropertyName().equals("loadData"))
-    {
-        System.out.println("loading data");
-        Platform.runLater(() -> {
-            connectedUsers.set(model.getConnectedUsers());
-            username.set(model.getUsername());
-            ip.set(model.getIP());
-        });
-    }
-      else if (evt.getPropertyName().equals("message")){
-          addToList((Message) evt.getNewValue());
+    public StringProperty usernameProperty() {
+        return username;
     }
 
-  }
-
-  private ObservableList<TableRowData> createList()
-  {
-    ObservableList<TableRowData> list = FXCollections.observableArrayList();
-
-    ArrayList<Message> messages = new ArrayList<>();
-    messages.addAll(model.getMessages(50));
-
-    for (int i = 0; i < messages.size(); i++)
-    {
-      list.add(new TableRowData(messages.get(i)));
-    }
-    return list;
-  }
-
-    public ObservableList<TableRowData> getList(){
-      return list;
-    }
-
-    public StringProperty ipProperty() {
-        return ip;
+    public IntegerProperty connectedUsersProperty() {
+        return connectedUsers;
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("connectedUpdate")) {
             Platform.runLater(() -> connectedUsers.set(model.getConnectedUsers()));
-        } else if(evt.getPropertyName().equals("loadData")) {
+        } else if (evt.getPropertyName().equals("loadData")) {
             System.out.println("loading data");
             Platform.runLater(() -> {
                 connectedUsers.set(model.getConnectedUsers());
                 username.set(model.getUsername());
                 ip.set(model.getIP());
             });
+        } else if (evt.getPropertyName().equals("message")) {
+            addToList((Message) evt.getNewValue());
         }
 
-
-    public void addToList(Message message){
-      Platform.runLater(()->{
-          list.add(0,new TableRowData(message));
-          if(list.size()>50){
-              list.remove(list.size()-1);
-          }
-      });
     }
 
+    private ObservableList<TableRowData> createList() {
+        ObservableList<TableRowData> list = FXCollections.observableArrayList();
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.addAll(model.getMessages(50));
+
+        for (int i = 0; i < messages.size(); i++) {
+            list.add(new TableRowData(messages.get(i)));
+        }
+        return list;
+    }
+
+    public ObservableList<TableRowData> getList() {
+        return list;
+    }
+
+    public StringProperty ipProperty() {
+        return ip;
+    }
+
+
+    public void addToList(Message message) {
+        Platform.runLater(() -> {
+            list.add(0, new TableRowData(message));
+            if (list.size() > 50) {
+                list.remove(list.size() - 1);
+            }
+        });
+    }
 }
 
