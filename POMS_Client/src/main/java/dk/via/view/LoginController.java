c7af2adc2f4a18b2cb1e1120cbcd800023ccd818
Paul <bujor.paul2000@gmail.com>
2020-03-28 14:14:29 +0100

    Update LoginController.java

--- POMS_Client/src/main/java/dk/via/view/LoginController.java
+++ POMS_Client/src/main/java/dk/via/view/LoginController.java
@@ -1,86 +1,71 @@
 package dk.via.view;
 
-import java.awt.*;
 import java.io.IOException;
 
 import dk.via.App;
+import dk.via.utility.StringIntegerConverter;
 import dk.via.viewmodel.LoginViewModel;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.util.StringConverter;
 import javafx.util.converter.NumberStringConverter;
 
-public class LoginController
-{
-  @FXML private TextField IPAddressField;
+public class LoginController {
+    @FXML
+    private TextField IPAddressField;
 
-  @FXML private TextField portLoginField;
+    @FXML
+    private TextField portLoginField;
 
-  @FXML private TextField userNameLoginField;
+    @FXML
+    private TextField userNameLoginField;
 
-  @FXML private Button connectButton;
+    @FXML
+    private Button connectButton;
 
-  @FXML private Label errorLabel = null;
+    @FXML
+    private Label errorLabel = null;
 
+    private Region root;
+    private ViewHandler viewHandler;
+    private LoginViewModel loginViewModel;
 
-  private Region root;
-  private ViewHandler viewHandler;
-  private LoginViewModel loginViewModel;
+    public LoginController() {
+    }
 
-  public LoginController()
-  {
-  }
-
-    public void init(ViewHandler viewHandler, LoginViewModel loginViewModel, Region root) {
+    public void init(ViewHandler viewHandler, LoginViewModel loginViewModel,
+                     Region root) {
         this.viewHandler = viewHandler;
         this.loginViewModel = loginViewModel;
         this.root = root;
+        IPAddressField.textProperty()
+                .bindBidirectional(loginViewModel.hostProperty());
 
-
-
-    }
-
-    public void reset() {
-
-    }
-
-
-  public void init(ViewHandler viewHandler, LoginViewModel loginViewModel,
-      Region root)
-  {
-    this.viewHandler = viewHandler;
-    this.loginViewModel = loginViewModel;
-    this.root = root;
-    IPAddressField.textProperty()
-        .bindBidirectional(loginViewModel.hostProperty());
-
-    Bindings.bindBidirectional(portLoginField.textProperty(),
-        loginViewModel.hostProperty().get(),
-
+        Bindings.bindBidirectional(portLoginField.textProperty(),
+                loginViewModel.portProperty(), new StringIntegerConverter(0));
 
         loginViewModel.connectedProperty().addListener((evt) -> {
             if (loginViewModel.connectedProperty().get() == true)
                 viewHandler.openView("chat");
             else
                 errorLabel.setText("Connection to the server failed...");
-        });
-  }
+        }));
+    }
 
-  public void reset()
-  {
-  }
+    public void reset() {
+    }
 
-  public Region getRoot()
-  {
-    return root;
-  }
+    public Region getRoot() {
+        return root;
+    }
 
-  @FXML private void connectButtonPressed() throws IOException
-  {
-    loginViewModel.connect();
-  }
+    @FXML
+    private void connectButtonPressed() throws IOException {
+        loginViewModel.connect();
+    }
 
 }
